9ff3c102c279c53e7064731457ae2ca0c48cfc53
Guilherme Camposo <guilherme.camposo@me.com>
2025-05-06 11:10:57 -0400

    updatings quarkus and java version of examples

--- apps/consumer/src/main/java/org/acme/kafka/processor/QuotesProcessor.java
+++ apps/consumer/src/main/java/org/acme/kafka/processor/QuotesProcessor.java
@@ -2,13 +2,13 @@ package org.acme.kafka.processor;
 
 import java.util.Random;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.acme.kafka.model.Quote;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import jakarta.enterprise.context.ApplicationScoped;
 
 /**
  * A bean consuming data from the "quote-requests" Kafka topic (mapped to "requests" channel) and giving out a random quote.
@@ -19,10 +19,15 @@ public class QuotesProcessor {
 
     private Random random = new Random();
 
+     private static final Logger LOGGER = Logger.getLogger(QuotesProcessor.class);
+
     @Incoming("requests")
     @Outgoing("quotes")
     @Blocking
     public Quote process(String quoteRequest) throws InterruptedException {
+
+        LOGGER.info("Processsing Quote request...");
+
         // simulate some hard working task
         Thread.sleep(200);
         return new Quote(quoteRequest, random.nextInt(100));
---
+++ apps/producer/src/main/java/org/acme/kafka/producer/MoviesResource.java
@@ -0,0 +1,27 @@
+package org.acme.kafka.producer;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.Path;
+import org.jboss.logging.Logger;
+
+import org.acme.kafka.quarkus.Movie;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+// @Path("/movies")
+public class MoviesResource {
+
+  // private static final Logger LOGGER = Logger.getLogger(MoviesResource.class);
+
+  // @Channel("movies")
+  // Emitter<Movie> emitter;
+
+  // @POST
+  // public Response enqueueMovie(Movie movie) {
+  //     LOGGER.infof("Sending movie %s to Kafka", movie.getTitle());
+  //     emitter.send(movie);
+  //     return Response.accepted().build();
+  // }
+    
+}
